dac3d20e7acb53160410ad0b1f98806d9197db16
nicka <nickaldred@hotmail.co.uk>
2024-03-16 11:13:27 +0000

    Initiaslised the sync processor

---
+++ src/sync_processor.py
@@ -0,0 +1,30 @@
+"""Syncs the board and calendar."""
+
+
+class SyncProcessor:
+    """Syncs the board and calendar"""
+
+    def __init(self):
+        pass
+
+    def sync_calendar(
+        self,
+    ):
+        """Syncs the calendar with the board events"""
+
+        # get board events
+        # get calendar events
+        # check they match
+        # make any adjustments
+
+    def get_board_events(self):
+        """Gets the events from the board."""
+
+    def get_calendar_events(self):
+        """Gets the events from the calendar."""
+
+    def compare_events(self):
+        """Compares the events to check if they are in sync."""
+
+    def sync_up_events(self):
+        """Syncs up the out of sync board and calendar events."""
