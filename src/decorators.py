b607b7e772fbc3e2071461d62e55672fee241abd
nicka <nickaldred@hotmail.co.uk>
2024-03-29 11:56:39 +0000

    Tidied up logging code

--- src/decorators.py
+++ src/decorators.py
@@ -1,52 +1,67 @@
 import logging
 import functools
-import json_log_formatter
+from typing import Any, Optional
+from json_log_formatter import JSONFormatter
 from datetime import datetime
 
 
-class CustomisedJSONFormatter(json_log_formatter.JSONFormatter):
-    def json_record(self, message, extra, record):
-        return super(CustomisedJSONFormatter, self).json_record(
-            message, extra, record
-        )
+class CustomisedJSONFormatter(JSONFormatter):
+    """Customised JSON formatter."""
+
+    def json_record(
+        self, message: str, extra: dict, record: logging.LogRecord
+    ) -> dict:
+        """Create a JSON log record."""
+        return {
+            "time_stamp": datetime.now().isoformat(),
+            "log_level": record.levelname,
+            **extra,
+            "message": message,
+        }
 
 
-formatter = CustomisedJSONFormatter()
+def get_logger() -> logging.Logger:
+    """Get a logger.
 
-json_handler = logging.FileHandler(
-    filename="/home/nicka/projects/cal_sync/logs/cal_sync_logs.log"
-)
-json_handler.setFormatter(formatter)
+    Returns:
+        logging.Logger: Logger
+    """
 
-logger = logging.getLogger(__name__)
-logger.addHandler(json_handler)
-logger.setLevel(logging.INFO)
+    formatter: CustomisedJSONFormatter = CustomisedJSONFormatter()
 
+    json_handler = logging.FileHandler(
+        filename="/home/nicka/projects/cal_sync/logs/cal_sync_logs.log"
+    )
+    json_handler.setFormatter(formatter)
 
-def get_logger():
+    logger = logging.getLogger(__name__)
+    logger.addHandler(json_handler)
+    logger.setLevel(logging.INFO)
     return logger
 
 
+LOGGER: logging.Logger = get_logger()
+
+
 def log_decorator(func):
+    """Decorator to log the start and end of a function execution."""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        item_id = kwargs.pop("item_id", None)
-        logger.info(
+
+        item_id: Any = kwargs.pop("item_id", None)
+        LOGGER.info(
             "execution_started",
             extra={
-                "time_stamp": datetime.now(),
                 "item_id": item_id,
-                "log_level": "INFO",
                 "function_name": func.__name__,
             },
         )
-        result = func(*args, **kwargs)
-        logger.info(
+        result: Any = func(*args, **kwargs)
+        LOGGER.info(
             "execution_ended",
             extra={
-                "time_stamp": datetime.now(),
                 "item_id": item_id,
-                "log_level": "INFO",
                 "function_name": func.__name__,
             },
         )
@@ -56,29 +71,24 @@ def log_decorator(func):
 
 
 def debug_log_decorator(func):
+    """Decorator to debug log the start and end of a function execution."""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        item_id = kwargs.pop("item_id", None)
-        logger.debug(
-            {
-                "event": "execution_started",
-            },
+
+        item_id: Any = kwargs.pop("item_id", None)
+        LOGGER.debug(
+            "execution_started",
             extra={
-                "time_stamp": datetime.now(),
                 "item_id": item_id,
-                "log_level": "DEBUG",
                 "function_name": func.__name__,
             },
         )
-        result = func(*args, **kwargs)
-        logger.debug(
-            {
-                "event": "execution_ended",
-            },
+        result: Any = func(*args, **kwargs)
+        LOGGER.debug(
+            "execution_ended",
             extra={
-                "time_stamp": datetime.now(),
                 "item_id": item_id,
-                "log_level": "DEBUG",
                 "function_name": func.__name__,
             },
         )
@@ -87,13 +97,24 @@ def debug_log_decorator(func):
     return wrapper
 
 
-def log_error(message, error_type, item_id=None):
-    logger.error(
+def log_error(
+    message: str, error_type: str, item_id: Optional[str] = None
+) -> None:
+    """Log an error message.
+
+    Args:
+        message (str): Error message
+        error_type (str): Error type
+        item_id (Optional[str], optional): Item ID. Defaults to None.
+
+    return:
+        None
+    """
+
+    LOGGER.error(
         message,
         extra={
-            "time_stamp": datetime.now(),
             "item_id": item_id,
-            "log_level": "ERROR",
             "error_type": error_type,
             "function_name": "log_error",
         },
@@ -105,10 +126,10 @@ def test(item_id=None):
 
     try:
         result = 1 + 2
-        raise KeyError
+        # raise KeyError
     except KeyError as e:
         log_error("KeyError occurred", error_type="KeyError", item_id=item_id)
         raise e
 
 
-print(test(item_id=3))
+test(item_id=1)
