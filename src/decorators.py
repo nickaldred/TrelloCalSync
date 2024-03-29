16972306ee9bd3b3e39c5c190e0808fe24117fe1
nicka <nickaldred@hotmail.co.uk>
2024-03-29 10:57:47 +0000

    logging decorators working

--- src/decorators.py
+++ src/decorators.py
@@ -3,38 +3,78 @@ import functools
 import json_log_formatter
 from datetime import datetime
 
+
 class CustomisedJSONFormatter(json_log_formatter.JSONFormatter):
     def json_record(self, message, extra, record):
-        extra['timestamp'] = datetime.now().isoformat()
-        return super(CustomisedJSONFormatter, self).json_record(message, extra, record)
+        extra["timestamp"] = datetime.now().isoformat()
+        return super(CustomisedJSONFormatter, self).json_record(
+            message, extra, record
+        )
+
 
 formatter = CustomisedJSONFormatter()
 
-json_handler = logging.FileHandler(filename='/path/to/your/logfile.log')
+json_handler = logging.FileHandler(
+    filename="/home/nicka/projects/cal_sync/logs/cal_sync_logs.log"
+)
 json_handler.setFormatter(formatter)
 
 logger = logging.getLogger(__name__)
 logger.addHandler(json_handler)
 logger.setLevel(logging.INFO)
 
+
 def get_logger():
     return logger
+
 
 def log_decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        logger.info({'event': 'execution_started', 'name': func.__name__})
+        logger.info(
+            {
+                "function_name": func.__name__,
+                "event": "execution_started",
+            }
+        )
         result = func(*args, **kwargs)
-        logger.info({'event': 'execution_ended', 'name': func.__name__})
+        logger.info(
+            {
+                "function_name": func.__name__,
+                "event": "execution_ended",
+            }
+        )
         return result
+
     return wrapper
+
 
 def debug_log_decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        logger.debug({'event': 'execution_started', 'name': func.__name__})
+        logger.debug(
+            {
+                "function_name": func.__name__,
+                "event": "execution_started",
+            }
+        )
         result = func(*args, **kwargs)
-        logger.debug({'event': 'execution_ended', 'name': func.__name__})
+        logger.debug(
+            {
+                "function_name": func.__name__,
+                "event": "execution_ended",
+            }
+        )
         return result
+
     return wrapper
 
+
+@log_decorator
+def test():
+
+    result = 1 + 2
+    return result
+
+
+print(test())
