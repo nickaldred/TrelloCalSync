3458d7db05a64858cdd66c6b7a6a6220ad9e3019
nicka <nickaldred@hotmail.co.uk>
2024-04-01 16:32:09 +0100

    Duration working

--- trello_powerup/client.js
+++ trello_powerup/client.js
@@ -1,16 +1,37 @@
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
-var addToCalendar= function (t, date) {
-  console.log("Adding to calendar: " + date);
-  
+var addToCalendar= function (t, date, duration) {
+    console.log("Adding to calendar: " + date);
+    console.log("Duration: " + duration);
 };
 
-var btnCallback = function (t) {
+var getDuration= function (t, date) {
+  console.log("Adding to calendar: " + date);
+
     return t.popup({
+        title: 'Choose duration',
+        items: [{
+        text: '1 hour',
+        callback: function (t, opts) {
+            return addToCalendar(t, date, 1);
+        }}, 
+        {
+        text: '2 hours',
+        callback: function (t, opts) { return addToCalendar(t, date, 2);},
+        },
+        {
+        text: 'All day',
+        callback: function (t, opts) { return addToCalendar(t, date, 24);}
+        }]
+    });
+    };
+
+var btnCallback = function (t) {
+    calendar_event_details = t.popup({
         title: 'Pick a date and time',
         type: 'datetime',
         callback: function(t, opts) {
-            return addToCalendar(t, opts.date);
+            return getDuration(t, opts.date);
         },
         date: Date,
       })};
