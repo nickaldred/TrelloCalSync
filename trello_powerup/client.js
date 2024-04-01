3ac46ee536eed81ab7fda85b12b7427b3a9c40ae
nicka <nickaldred@hotmail.co.uk>
2024-04-01 16:48:42 +0100

    Added docs to explain whats happening in file

--- trello_powerup/client.js
+++ trello_powerup/client.js
@@ -1,3 +1,5 @@
+// This file contains the client-side code for the Trello Power-Up.
+
 var GRAY_ICON =
   "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg";
 
